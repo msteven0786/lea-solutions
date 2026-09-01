83bed3c67ba0ecd43e4ace27282c3fe5b5e97643
msteven0786 <msteven0786@gmail.com>
2026-09-01 08:19:41 +0100

    Fix Netlify form submission target

--- components/AutomationReviewForm.tsx
+++ components/AutomationReviewForm.tsx
@@ -27,7 +27,7 @@ export default function AutomationReviewForm() {
     });
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/__forms.html", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
